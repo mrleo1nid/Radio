c0afdc3ac65ddbb2f2b5f86366b9960587a8775e
mrleo1nid <43583385+mrleo1nid@users.noreply.github.com>
2019-01-20 14:51:05 +0300

    fuck

---
+++ Radio/Behaviors/PlaylistBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using Radio.ViewModels;
+using Radio.Views.UserControls;
+using WPFSoundVisualizationLib;
+
+namespace Radio.Behaviors
+{
+    class PlaylistBehavior : Behavior<PlaylistView>
+    {
+
+
+        public static readonly DependencyProperty PlaylistBehaviorProperty =
+            DependencyProperty.Register(
+                nameof(PlaylistBehavior),
+                typeof(string),
+                typeof(PlaylistBehavior));
+
+        public string SpectrumAnalyzerFieldBehavior
+        {
+            get { return (string) GetValue(PlaylistBehaviorProperty); }
+            set { SetValue(PlaylistBehaviorProperty, value); }
+        }
+
+        private void AssociatedObject_Initialized(object sender, EventArgs e)
+        {
+            var associatedObject = sender as PlaylistView;
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Initialized += AssociatedObject_Initialized;
+            AssociatedObject.MouseEnter += AssociatedObjectOnMouseEnter;
+            AssociatedObject.MouseDown += AssociatedObjectOnMouseDown;
+        }
+
+        private void AssociatedObjectOnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var playlist = sender as PlaylistView;
+            if (playlist.Visibility == Visibility.Visible)
+            {
+                playlist.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void AssociatedObjectOnMouseEnter(object sender, MouseEventArgs e)
+        {
+            var playlist = sender as PlaylistView;
+            if (playlist.Visibility == Visibility.Hidden)
+            {
+                playlist.Visibility = Visibility.Visible;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+        }
+    }
+}
